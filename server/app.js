2218265b0edf73f9a4b455db494c39f72ed06a5d
Nikhil Sharma <98894357+d-e-a-d-s-p-i-r-i-t@users.noreply.github.com>
2025-02-28 06:06:13 +0530

    changes1

--- server/app.js
+++ server/app.js
@@ -1,36 +1,31 @@
-require('dotenv').config()
-const express = require("express")
-const mysql = require("mysql2")
-const path = require('path')
-const router=require('./router/router')
-const cors=require('cors')
+require("dotenv").config();
+const express = require("express");
+const mysql = require("mysql2");
+const path = require("path");
+const router = require("./router/router");
+const cors = require("cors");
 
-const app = express()
-app.use(cors({
-    origin:['https://asc-hirehub.netlify.app','http://127.0.0.1:5502']
-}))
+const app = express();
+app.use(cors());
 
-const connectDB = require('./connectionDB/connectionDB')
-const { link } = require("fs")
+const connectDB = require("./connectionDB/connectionDB");
+const { link } = require("fs");
 
 //middlewares
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(express.static('./public'))
-app.use(express.static('./public/main-page'))
-app.use('/',router)
-
-
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("./public"));
+app.use(express.static("./public/main-page"));
+app.use("/", router);
 
 //main-page
-app.get('/mainpage', (req, res) => {//don't use '/main-page' because this will give r=error since '/main-page' is a folder
-    return res.status(200).redirect('./main-page/index1.html')//as frontend request /index1.html server redirects to ./main-page/index1.html'
-    // res.sendFile(path.resolve(__dirname,'./public/main-page/index1.html'))//as frontend request /index1.html server send this file
-})
+app.get("/mainpage", (req, res) => {
+  //don't use '/main-page' because this will give r=error since '/main-page' is a folder
+  return res.status(200).redirect("./main-page/index1.html"); //as frontend request /index1.html server redirects to ./main-page/index1.html'
+  // res.sendFile(path.resolve(__dirname,'./public/main-page/index1.html'))//as frontend request /index1.html server send this file
+});
 
-
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log("the server is listening at ",port)
-})
+  console.log("the server is listening at ", port);
+});
